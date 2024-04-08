07a1dc06eace81cc7148e8206d79fa286dd7e19d
Ruby Kendrick <kendrick.ruby@gmail.com>
2024-04-08 11:30:18 -0500

    display the number of guesses the user has left and generate secret number randomly

--- Program.cs
+++ Program.cs
@@ -1,15 +1,21 @@
 ﻿//write a readline that takes an integer between 1 and 100, if not we prompt again
 //with a while loop wrap in a function called get guess
 
-int secretNumber = 69;
+int secretNumber = 69420;
 
 void Main()
 {
     Console.WriteLine("Welcome to Guessing Game!!!!!!!!!!!!!!!!");
     Console.WriteLine("Guess the number between 1 and 100");
 
-    for (int i = 1; i <= 4; i++)
+    Random random = new Random();
+    secretNumber = random.Next()%100+1;
+    Console.WriteLine($"{secretNumber}");
+    int maxGuesses = 4;
+    for (int i = 1; i <= maxGuesses; i++)
     {
+        Console.WriteLine($"You have {maxGuesses-i+1} {(maxGuesses-i+1 == 1 ? "guess" : "guesses")} ");
+
         int userGuess = GetGuess(i);
         if (DisplayMessageForSecretNumber(userGuess))
         {
