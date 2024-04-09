0413a037161f1197c663d589eee82d855635aa18
Ruby Kendrick <kendrick.ruby@gmail.com>
2024-04-09 09:35:55 -0500

    allow user to choose a difficulty level

--- Program.cs
+++ Program.cs
@@ -9,12 +9,12 @@ void Main()
     Console.WriteLine("Guess the number between 1 and 100");
 
     Random random = new Random();
-    secretNumber = random.Next()%100+1;
+    secretNumber = random.Next() % 100 + 1;
     Console.WriteLine($"{secretNumber}");
-    int maxGuesses = 4;
+    int maxGuesses = SetDifficultyLevel();
     for (int i = 1; i <= maxGuesses; i++)
     {
-        Console.WriteLine($"You have {maxGuesses-i+1} {(maxGuesses-i+1 == 1 ? "guess" : "guesses")} ");
+        Console.WriteLine($"You have {maxGuesses - i + 1} {(maxGuesses - i + 1 == 1 ? "guess" : "guesses")} ");
 
         int userGuess = GetGuess(i);
         if (DisplayMessageForSecretNumber(userGuess))
@@ -60,20 +60,68 @@ bool DisplayMessageForSecretNumber(int guess)
         );
         return true;
     }
-    else if(guess > secretNumber)
+    else if (guess > secretNumber)
     {
         Console.BackgroundColor = ConsoleColor.Gray;
         Console.WriteLine("Hey dummy your guess was to high");
         return false;
     }
-    else 
+    else
     {
         Console.BackgroundColor = ConsoleColor.Gray;
         Console.WriteLine("Hey dummy your guess was to low");
         return false;
     }
-    
-    
+
+
+}
+
+//function called set DIfficulty tha prompts the user to pick a difficulty
+//display menu options
+// if statement to set the guesses variable based on the users choice
+//return that integer
+
+int SetDifficultyLevel()
+{
+    Console.WriteLine(@"Please enter THE NUMBER that corresponds with your preferred difficulty level:
+    1. Easy
+    2. Medium
+    3. Hard");
+    int userChoice = 0;
+    int maxGuesses = 0;
+    while (userChoice == 0)
+    {
+        try
+        {
+            userChoice = int.Parse(Console.ReadLine().Trim());
+        }
+        catch
+        {
+            Console.WriteLine("NOT A NUMBER YALL");
+            continue;
+        }
+        if (!(userChoice >= 1 && userChoice <= 3))
+        {
+            Console.WriteLine("please choose a valid option");
+            userChoice = 0;
+        }
+
+    }
+
+    if (userChoice == 1)
+    {
+        maxGuesses = 8;
+    }
+    if (userChoice == 2)
+    {
+        maxGuesses = 6;
+    }
+    if (userChoice == 3)
+    {
+        maxGuesses = 4;
+    }
+
+    return maxGuesses;
 }
 
 Main();
